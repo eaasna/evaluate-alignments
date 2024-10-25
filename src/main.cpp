c63c7e6d48de03fe6c86a38301a3d6fd39b31fb9
Evelin <evelin.aasna@fu-berlin.de>
2024-10-25 11:44:18 +0200

    Test data

--- src/main.cpp
+++ src/main.cpp
@@ -24,20 +24,20 @@ int main(int argc, char ** argv)
     parser.add_option(args.truth_file,
                       sharg::config{.short_id = '\0',
                                     .long_id = "truth",
-                                    .required = true,
                                     .description = "The ground truth.",
+                                    .required = true,
                                     .validator = sharg::input_file_validator{{"gff", "txt"}}});
     parser.add_option(args.test_file,
                       sharg::config{.short_id = '\0',
                                     .long_id = "test",
-                                    .required = true,
                                     .description = "The alignments to evaluate.",
+                                    .required = true,
                                     .validator = sharg::input_file_validator{{"gff", "txt"}}});
     parser.add_option(args.ref_meta,
                       sharg::config{.short_id = '\0',
                                     .long_id = "ref-meta",
-                                    .required = true,
                                     .description = "The reference metadata from valik split.",
+                                    .required = true,
                                     .validator = sharg::input_file_validator{{}}});
     parser.add_option(args.min_len,
                       sharg::config{.short_id = 'l',
@@ -52,8 +52,8 @@ int main(int argc, char ** argv)
     parser.add_option(args.out_file,
                       sharg::config{.short_id = 'o',
                                     .long_id = "out",
-                                    .description = "The missed alignments.",
-                                    .validator = sharg::output_file_validator{{"gff", "txt"}}});
+                                    .description = "Search accuracy report.",
+                                    .validator = sharg::output_file_validator{}});
     parser.add_flag(args.verbose,
                     sharg::config{.short_id = 'v',
                                   .long_id = "verbose", 
--- src/search_accuracy.cpp
+++ src/search_accuracy.cpp
@@ -17,16 +17,15 @@ void runtime_to_compile_time(func_t const & func, bool b1)
 void search_accuracy(accuracy_arguments const & arguments)
 {
     valik::metadata meta(arguments.ref_meta);    
-
     runtime_to_compile_time([&]<bool is_gff>()
     {
         auto truth = get_alignments<is_gff>(arguments.truth_file, meta);
         seqan3::debug_stream << "Truth matches\t" << truth.size() << '\n';
-    }, (arguments.truth_file.extension() == "gff"));
+    }, (arguments.truth_file.extension() == ".gff"));
 
     runtime_to_compile_time([&]<bool is_gff>()
     {
         auto test = get_alignments<is_gff>(arguments.test_file, meta);
         seqan3::debug_stream << "Test matches\t" << test.size() << '\n';
-    }, (arguments.test_file.extension() == "gff"));
+    }, (arguments.test_file.extension() == ".gff"));
 }
